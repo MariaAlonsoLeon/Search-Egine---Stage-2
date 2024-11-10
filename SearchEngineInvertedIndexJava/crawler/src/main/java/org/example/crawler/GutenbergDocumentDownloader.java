04aab2ca4b633b4764e7b9d279a50de5ce89d2af
MariaAlonsoLeon <145381435+MariaAlonsoLeon@users.noreply.github.com>
2024-11-10 22:39:11 +0000

    Update GutenbergDocumentDownloader.java

--- SearchEngineInvertedIndexJava/crawler/src/main/java/org/example/crawler/GutenbergDocumentDownloader.java
+++ SearchEngineInvertedIndexJava/crawler/src/main/java/org/example/crawler/GutenbergDocumentDownloader.java
@@ -1,18 +1,40 @@
 package org.example.crawler;
 
+import java.io.IOException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import java.io.IOException;
+import org.jsoup.HttpStatusException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.URI;
 
 public class GutenbergDocumentDownloader implements DocumentDownloader {
-    private static final String URL_PROJECT_GUTENBERG = "https://www.gutenberg.org/cache/epub/{id}/pg{id}.txt";
+    private static final String URL_PROJECT_GUTENBERG = "https://www.gutenberg.org/cache/epub/%d/pg%d.txt";
 
     @Override
     public Document downloadDocument(int bookId) throws IOException {
-        String bookUrl = URL_PROJECT_GUTENBERG.replace("{id}", String.valueOf(bookId));
-        System.out.println("Attempting to download document from: " + bookUrl);
-        return Jsoup.connect(bookUrl)
-                .userAgent("Mozilla/5.0")
-                .timeout(10000)
-                .get();    }
+        String bookUrl = String.format(URL_PROJECT_GUTENBERG, bookId, bookId);
+        System.out.println("Downloading from: " + bookUrl);
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(bookUrl))
+                .build();
+
+        try {
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            if (response.statusCode() == 200) {
+                String documentText = response.body();
+                return Jsoup.parse(documentText);  // Analiza y devuelve el contenido como un objeto Document de Jsoup
+            } else {
+                throw new HttpStatusException("Error en la descarga", response.statusCode(), bookUrl);
+            }
+        } catch (InterruptedException e) {
+            System.err.println("Request interrupted: " + e.getMessage());
+            Thread.currentThread().interrupt();
+            throw new IOException("Error en la descarga", e);
+        }
+    }
 }
