2d09109c1670e8a9a77aa02cbf02d103835ede40
loseg <loprimeroquepense011@gmail.com>
2024-11-13 23:12:02 +0000

    Query Example

---
+++ SearchEngineInvertedIndexJava/queryengine2/src/main/java/org/example/all/QueryExample.java
@@ -0,0 +1,61 @@
+package org.example.all;
+
+import org.example.InvertedIndex.TXT.ContextSearchFile;
+import org.main.inverted_index.StoreInterface_II;
+import org.main.inverted_index.StoreNeo4j_II;
+import org.neo4j.driver.AuthTokens;
+import org.neo4j.driver.Driver;
+import org.neo4j.driver.GraphDatabase;
+import org.example.InvertedIndex.TXT.SingleWordSearchFile;
+
+import java.util.*;
+
+public class QueryExample {
+    public static void main(String[] args) {
+        Driver driver = GraphDatabase.driver("neo4j://localhost:7687", AuthTokens.basic("neo4j", "unodostres"));
+        StoreInterface_II store = new StoreNeo4j_II();
+        //QueryEngine queryEngine = new Neo4jQueryEngine(store);
+
+        // Búsqueda de una palabra
+        /*SearchCommand singleWordSearch = new SingleWordSearchNeo4J("near", driver);
+        List<String> singleWordResults = queryEngine.search(singleWordSearch);
+
+        // Búsqueda de palabras en conjunción
+
+        // Imprimir resultados
+        System.out.println("Resultados de búsqueda para 'amor': " + singleWordResults);*/
+
+        //MongoDB_II mongo = new MongoDB_II();
+
+        //System.out.println(mongo.searchWord("ten"));
+
+        //SearchWordWithContextNeo4J single = new SearchWordWithContextNeo4J("prepare", driver, "datalake", 5);
+        //SearchByAuthorNeo4J single = new SearchByAuthorNeo4J("John F. Kennedy", driver);
+        //SearchByDate single = new SearchByDate("November 1, 1973 [eBook #3]", driver);
+        //SearchByLanguage single = new SearchByLanguage("English", driver);
+        //SingleWordSearchMongoDB single = new SingleWordSearchMongoDB("prepare");
+        //ContextSearchMongo single = new ContextSearchMongo("prepare", 5, "datalake");
+        //SearchByAuthorMongo single = new SearchByAuthorMongo("Thomas Jefferson");
+        //SearchByDateMongo single = new SearchByDateMongo("December 1, 1971 [eBook #1]");
+        //SearchByLanguageMongo single = new SearchByLanguageMongo("English");
+        //FetchMetadataByBookNameMongo single = new FetchMetadataByBookNameMongo("datalake/20241111/book_1.txt");
+        //FetchMetadataByBookNameNeo4j single = new FetchMetadataByBookNameNeo4j("datalake/20241111/book_1.txt");
+        //System.out.println(single.execute());
+
+        /*List<String> keywords = List.of("");  // Palabras clave
+        String author = "Thomas Jefferson";  // Filtro por autor
+        String startDate = "1900-01-01";  // Fecha de inicio
+        String endDate = "2025-12-31";  // Fecha de fin
+        String language = "English";  // Idioma
+
+        // Ejecutar la búsqueda booleana
+        BooleanSearchMongoDB search = new BooleanSearchMongoDB(keywords, author, startDate, endDate, language);
+        System.out.println(search.execute());*/
+
+        //SingleWordSearchBinary single = new SingleWordSearchBinary("prepare");
+        //SingleWordSearchFile single = new SingleWordSearchFile("near", "datamart/inverted_index.txt");
+        ContextSearchFile single = new ContextSearchFile("near", "datamart/inverted_index.txt");
+        System.out.println(single.execute());
+
+    }
+}
