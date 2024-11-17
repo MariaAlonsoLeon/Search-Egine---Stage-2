4cb91bc4bb868df655d67e692352d33b7210d6d9
loseg <loprimeroquepense011@gmail.com>
2024-11-17 13:56:10 +0000

    Main update

--- SearchEngineInvertedIndexJava/indexer/src/main/java/org/main/Main.java
+++ SearchEngineInvertedIndexJava/indexer/src/main/java/org/main/Main.java
@@ -19,44 +19,20 @@ public class Main {
     private static String currentDate;
     private static final ProcessMetadata process_metadata = new ProcessMetadata();
     private static final ProcessInvertedIndex process_inverted_index = new ProcessInvertedIndex();
-    private static final StoreInterface_II storeTextII = new StoreText_II();
-    private static final StoreInterface_II storeBinaryFileII = new StoreBinary_II();
-    private static final StoreInterface_II store_mongoDB_II = new StoreMongoDB_II();
-    private static final StoreInterface_II storeNeo4jII = new StoreNeo4j_II();
-    private static final StoreInterface_MD storeBinaryFileMD = new StoreBinary_MD();
-    private static final StoreInterface_MD store_mongoDB_MD = new StoreMongoDB_MD();
-    private static final StoreInterface_MD storeNeo4jMD = new StoreNeo4j_MD();
-    private static final StoreInterface_MD storeTextMD = new StoreText_MD();
 
     public static void main(String[] args) {
+        // Obtener DATATYPE de la variable de entorno
         String datatype = System.getenv("DATATYPE");
         System.out.println("DATATYPE: " + datatype);
 
         updateDate();
         scheduleMidnightUpdate();
 
-        Map<String, Consumer<DocumentProcessors>> actions = new HashMap<>();
-        actions.put("Text", processors -> {
-            processors.storeInvertedIndex(storeTextII::storeInvertedIndex);
-            processors.storeMetadata(storeTextMD::storeMetadata);
-        });
-        actions.put("BinaryFile", processors -> {
-            processors.storeInvertedIndex(storeBinaryFileII::storeInvertedIndex);
-            processors.storeMetadata(storeBinaryFileMD::storeMetadata);
-        });
-        actions.put("MongoDB", processors -> {
-            processors.storeInvertedIndex(store_mongoDB_II::storeInvertedIndex);
-            processors.storeMetadata(store_mongoDB_MD::storeMetadata);
-        });
-        actions.put("NEO4J", processors -> {
-            processors.storeInvertedIndex(storeNeo4jII::storeInvertedIndex);
-            processors.storeMetadata(storeNeo4jMD::storeMetadata);
-        });
+        Map<String, Consumer<DocumentProcessors>> actions = defineActions();
 
         if (datatype != null && actions.containsKey(datatype)) {
             processAllDocumentsInDatalake(DOCUMENT_REPOSITORY, actions.get(datatype));
-
-            listener(DOCUMENT_REPOSITORY, storeNeo4jII, storeNeo4jMD);
+            startListener(DOCUMENT_REPOSITORY, actions.get(datatype));
         } else {
             System.out.println("Unsupported or undefined DATATYPE: " + datatype);
         }
@@ -88,54 +64,47 @@ public class Main {
         scheduler.scheduleAtFixedRate(Main::updateDate, initialDelay, period, TimeUnit.MILLISECONDS);
     }
 
+    private static Map<String, Consumer<DocumentProcessors>> defineActions() {
+        Map<String, Consumer<DocumentProcessors>> actions = new HashMap<>();
+
+        actions.put("Text", processors -> {
+            processors.storeInvertedIndex(new StoreText_II()::storeInvertedIndex);
+            processors.storeMetadata(new StoreText_MD()::storeMetadata);
+        });
+
+        actions.put("BinaryFile", processors -> {
+            processors.storeInvertedIndex(new StoreBinary_II()::storeInvertedIndex);
+            processors.storeMetadata(new StoreBinary_MD()::storeMetadata);
+        });
+
+        actions.put("MongoDB", processors -> {
+            processors.storeInvertedIndex(new StoreMongoDB_II()::storeInvertedIndex);
+            processors.storeMetadata(new StoreMongoDB_MD()::storeMetadata);
+        });
+
+        actions.put("NEO4J", processors -> {
+            processors.storeInvertedIndex(new StoreNeo4j_II()::storeInvertedIndex);
+            processors.storeMetadata(new StoreNeo4j_MD()::storeMetadata);
+        });
+
+        return actions;
+    }
 
     private static void processAllDocumentsInDatalake(String rootFolderPath, Consumer<DocumentProcessors> action) {
         try {
             Files.walk(Paths.get(rootFolderPath))
                     .filter(Files::isRegularFile)
-                    .forEach(path -> {
-                        String filePath = path.toAbsolutePath().toString();
-                        System.out.println("Processing file: " + filePath);
-
-                        Map<String, String> documentContent = readBook(filePath);
-
-                        if (documentContent.containsKey("error")) {
-                            System.out.println("Error reading file: " + documentContent.get("error"));
-                            return;
-                        }
-
-                        Map<String, Map<String, List<Integer>>> invertedIndex =
-                                process_inverted_index.createInvertedIndex(documentContent, "indexer\\src\\main\\resources\\stopwords.txt");
-                        Map<String, Map<String, String>> metadata =
-                                process_metadata.createMetadata(documentContent);
-
-                        DocumentProcessors processors = new DocumentProcessors(invertedIndex, metadata);
-                        action.accept(processors);
-
-                        System.out.println("Processing and storage completed for file: " + filePath);
-                    });
+                    .forEach(path -> processFile(path.toAbsolutePath().toString(), action));
         } catch (IOException e) {
-            System.out.println("Error traversing the datalake directory: " + e.getMessage());
+            System.err.println("Error traversing the datalake directory: " + e.getMessage());
             e.printStackTrace();
         }
     }
 
-    private static void listener(String folderPath_1, StoreInterface_II invertedIndexInterface, StoreInterface_MD metadataInterface) {
-        String folderPath = folderPath_1 + currentDate;
+    private static void startListener(String rootFolderPath, Consumer<DocumentProcessors> action) {
+        Path path = Paths.get(rootFolderPath, currentDate);
 
-        while (!new File(folderPath).exists()) {
-            System.out.println("Folder " + folderPath + " doesn't exist yet. Waiting for it...");
-            try {
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-                return;
-            }
-        }
-
-        System.out.println("Monitoring " + folderPath + " for new files...");
-        Path path = Paths.get(folderPath);
-
+        System.out.println("Initializing listener for: " + path);
         try (WatchService watchService = FileSystems.getDefault().newWatchService()) {
             path.register(watchService, StandardWatchEventKinds.ENTRY_CREATE);
 
@@ -151,34 +120,17 @@ public class Main {
                 for (WatchEvent<?> event : key.pollEvents()) {
                     WatchEvent.Kind<?> kind = event.kind();
 
-                    if (kind == StandardWatchEventKinds.OVERFLOW) {
-                        continue;
-                    }
+                    if (kind == StandardWatchEventKinds.OVERFLOW) continue;
 
                     WatchEvent<Path> ev = (WatchEvent<Path>) event;
-                    Path fileName = ev.context();
-
-                    String filePath = folderPath + "/" + fileName;
-                    System.out.println("New file detected: " + filePath);
-                    Thread.sleep(500);
-                    Map<String, String> bookContent = readBook(filePath);
-                    Map<String, Map<String, String>> metadata = process_metadata.createMetadata(bookContent);
-                    Map<String, Map<String, List<Integer>>> inverted_index = process_inverted_index.createInvertedIndex(bookContent, "indexer\\src\\main\\resources\\stopwords.txt");
-
-                    invertedIndexInterface.storeInvertedIndex(inverted_index);
-                    metadataInterface.storeMetadata(metadata);
+                    Path filePath = path.resolve(ev.context());
+                    processFile(filePath.toString(), action);
                 }
 
-                boolean valid = key.reset();
-                if (!valid) {
-                    break;
-                }
+                if (!key.reset()) break;
             }
-
         } catch (IOException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+            System.err.println("Error initializing the listener: " + e.getMessage());
         }
     }
 
@@ -186,7 +138,7 @@ public class Main {
         Map<String, String> documentContent = readBook(filePath);
 
         if (documentContent.containsKey("error")) {
-            System.out.println("Error reading file: " + documentContent.get("error"));
+            System.err.println("Error reading file: " + documentContent.get("error"));
             return;
         }
 
@@ -198,7 +150,7 @@ public class Main {
         DocumentProcessors processors = new DocumentProcessors(invertedIndex, metadata);
         action.accept(processors);
 
-        System.out.println("Processing and storage completed for file: " + filePath);
+        System.out.println("Processing completed for file: " + filePath);
     }
 
     private static Map<String, String> readBook(String fileName) {
@@ -207,19 +159,17 @@ public class Main {
 
         File file = new File(fileName);
         if (!file.exists() || !file.canRead()) {
-            System.out.println("File does not exist or cannot be read: " + fileName);
             result.put("error", "File does not exist or cannot be read.");
             return result;
         }
 
-        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
             String line;
             while ((line = br.readLine()) != null) {
                 content.append(line).append("\n");
             }
             result.put(fileName, content.toString());
         } catch (IOException e) {
-            e.printStackTrace();
             result.put("error", "Error reading file: " + e.getMessage());
         }
 
